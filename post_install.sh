65f9cf3c6c9634720ac5aa27155c2f0d9b30196b
fireph <443370+fireph@users.noreply.github.com>
2021-07-12 16:10:37 -0700

    Update for newest version of Sickchill that uses virtualenv

--- post_install.sh
+++ post_install.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 
 echo "Create location for sickchill"
-mkdir /usr/local/app
+mkdir -p /usr/local/app
 
 echo "Download sickchill repo"
 git clone https://github.com/SickChill/SickChill.git /usr/local/app/sickchill
 
-echo "Install dependencies"
-pip install -r /usr/local/app/sickchill/requirements.txt
+echo "Updating dependencies"
+pip install --upgrade pip
+pip install --upgrade virtualenv
 
 echo "Create sickchill user"
 pw user add sickchill -c "Sickchill" -d /nonexistent -s /usr/bin/nologin -w no
 chown -R sickchill:sickchill /usr/local/app/sickchill
 
+mkdir -p /.cargo
+chown -R sickchill:sickchill /.cargo
+
 echo "Installing SickChill service"
-mkdir /usr/local/etc/rc.d
+mkdir -p /usr/local/etc/rc.d
 cp /usr/local/app/sickchill/contrib/runscripts/init.freebsd /usr/local/etc/rc.d/sickchill
 
 echo "Executing SickChill service"
